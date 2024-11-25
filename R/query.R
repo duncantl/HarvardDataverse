0eac1ccc67dcfa0cb3457288600eb1b75575c090
Duncan Temple Lang <duncan@r-project.org>
2024-11-24 18:45:39 -0800

    mkDF(); added a wait parameter to slow the queries down.

--- R/query.R
+++ R/query.R
@@ -4,38 +4,82 @@ library(RCurl)
 library(RJSONIO)
 
 
+# See https://guides.dataverse.org/en/latest/api/search.html
+# for more parameters for the search query.
+
 getDOIs =
-function(url = "https://dataverse.harvard.edu/api/search",
-          lang = "r",  isHarvested = FALSE, max = Inf, perPage = 1000L, con = getCurlHandle(.opts = .opts), .opts = list(...))
+function(lang = "r",  isHarvested = FALSE, max = Inf, perPage = 1000L, con = getCurlHandle(.opts = .opts), .opts = list(...),
+         url = "https://dataverse.harvard.edu/api/search", wait = -1, start = 0L)
 {
-    
-    query = sprintf("(fileContentType:type/x-%s-syntax AND isHarvested:%s", lang, tolower(as.logical(isHarvested)))
-    getResults(url, query = query, start = 0, max = max, perPage = perPage)
+    getResults(url, query = mkQuery(lang, isHarvested), start = start, max = max, perPage = perPage)
 }
 
+mkQuery =
+function(lang = "r", isHarvested = FALSE)
+{
+    sprintf("(fileContentType:type/x-%s-syntax AND isHarvested:%s", lang, tolower(as.logical(isHarvested)))
+}
+
+getNumMatches =
+function(lang = "r", isHarvested = FALSE, url = "https://dataverse.harvard.edu/api/search")    
+{
+   ans = getPage(url, mkQuery(lang, isHarvested), 0, 10L)
+   ans$data$total_count 
+}
+
+
 getResults =
-function(url, query, start = 0L, max = Inf, perPage = 1000L, verbose = TRUE)    
+function(url, query, start = 0L, max = Inf, perPage = 1000L, verbose = TRUE, wait = -1)    
 {
     ans = list()
-browser()    
+
     while(start < max) {
 
         if(verbose)
             message("starting at ", start)
 
-        js = getForm(url, q = query, start = start, perPage = perPage)
-        v = fromJSON(js)
+        v = getPage(url, query, start, perPage)
 
         dt = v$data
         ans = c(ans, dt$items)
         start = start + dt$count_in_response
-browser()
+
         if(length(ans) >= dt$total_count)
             break
+
+        if(wait > 0)
+            Sys.sleep(wait)
     }
 
     invisible(ans)
 }
 
+getPage =
+function(url, query, start, perPage)    
+{
+    js = getForm(url, q = query, start = start, per_page = perPage)
+    v = fromJSON(js)
+}
 
 
+
+mkDF =
+function(x)    
+{
+    scalars = c("name", "type", "url", "file_id", "published_at", "file_type", "file_content_type",
+                "size_in_bytes", "md5", "dataset_name", "dataset_persistent_id",
+                 "dataset_citation", "publicationStatuses", "releaseOrCreateDate")
+
+    tmp = lapply(scalars, function(var) sapply(x, function(x) orNA(x[[var]])))
+    names(tmp) = scalars
+    as.data.frame(tmp)
+}
+
+orNA =
+function(x, val = NA)
+{
+    if(length(x))
+        x
+    else
+        val
+}
